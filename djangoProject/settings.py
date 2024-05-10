2ffa2de83f905ddad2ed50a1e5886cefb1aad8f2
Koray <ahmetkorayuzun61@gmail.com>
2024-05-11 01:00:35 +0300

    feat:enable in continuous development in docker

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -16,6 +16,10 @@ import environ
 env=environ.Env(DEBUG=(bool,False))
 environ.Env.read_env()
 
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
