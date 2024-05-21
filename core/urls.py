e289c6241324012a86912a6d6f00d29b42e3f458
Koray <ahmetkorayuzun61@gmail.com>
2024-05-21 17:57:27 +0300

    feat:create static-media tags and urls

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from.views import index,contact
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', index, name='index'),
     path('contact', contact, name='contact'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
